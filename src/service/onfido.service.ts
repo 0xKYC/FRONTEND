d840665ebaebd8bd4ce91d445ccd61c3ef5f7334
dwyso <76915554+WysockiD@users.noreply.github.com>
2022-12-05 13:40:13 +0100

    dApp Bootstrap

---
+++ src/service/onfido.service.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export async function getSDKToken() {
+  const generateSdkToken = await axios.post(
+    "http://localhost:3001/onfido/sdkToken",
+    {}
+  );
+  return generateSdkToken;
+}
+
+export async function onfidoRedirect() {
+  const res = await axios.post("http://localhost:3001/onfido/redirect", {});
+  console.log(res.data);
+  window.location.replace(res.data);
+}
