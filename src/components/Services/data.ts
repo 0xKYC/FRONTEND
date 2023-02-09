5ebeb7b7adea799311c55e926eba5845493ac1ed
ankerx <93661548+ankerx@users.noreply.github.com>
2023-02-09 12:01:22 +0100

    style: cards with services

--- src/components/Services/data.ts
+++ src/components/Services/data.ts
@@ -1,26 +1,35 @@
 export const servicesData = [
   {
-    title: "OKX Web3",
-    description:
-      "OKX is an innovative cryptocurrency exchange with advanced financial offerings.",
-    tags: ["CEX"],
-    href: "https://www.okx.com/",
-    img: "/img/OKX_logo-new.png",
-  },
-  {
     title: "IdentDefi",
     description:
-      "IdentDeFi provides a lightweight, protocol-agnostic architecture that focuses on open, secure, privacy-preserving compliance checks.",
+      "A secure way to access DeFi protocols powered by 0xKYC (amongst other services).",
     tags: ["DeFi"],
     href: "https://identdefi.xyz/",
     img: "/img/identdefi-new.png",
   },
+
   {
     title: "Cryptum",
     description:
-      "Cryptum provides the easiest and fastest way to bring blockchain to any platform, saving time & cost.",
+      "APIs and SKDs for blockchain integrations, powered by 0xKYC AML solution (coming soon).",
     tags: ["Software"],
     href: "https://cryptum.io/",
     img: "/img/cryptum-logo-new.png",
+  },
+  {
+    title: "CodeMonk",
+    description:
+      "Effortlessly build your engineering team with fast, hassle-free hiring. Ensure compliance by only working with approved individuals.",
+    tags: ["DeFi"],
+    href: "https://www.codemonk.ai/",
+    img: "/img/codemonk.png",
+  },
+  {
+    title: "Lomads",
+    description:
+      "Secure your DAO with top-of-line management tools, including 0xKYC identity verification protocol to help you vouch for your members.",
+    tags: ["DeFi"],
+    href: "https://www.lomads.xyz/",
+    img: "/img/lomads.png",
   },
 ];
