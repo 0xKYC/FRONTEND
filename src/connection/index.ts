02443a57d9574ece0e5acd6e7247990bfee926c6
ankerx <93661548+ankerx@users.noreply.github.com>
2023-03-07 13:53:40 +0100

    remove unused code

--- src/connection/index.ts
+++ src/connection/index.ts
@@ -6,18 +6,12 @@ import {
 
 import { configureChains, createClient } from "wagmi";
 import { goerli, polygonMumbai } from "wagmi/chains";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { scrollAlpha } from "../constans/chains";
 
 export const connectWalletId = process.env.REACT_APP_WALLET_CONNECT_ID || "";
 export const chains = [goerli, polygonMumbai, scrollAlpha];
 
 const { provider } = configureChains(chains, [
-  // jsonRpcProvider({
-  //   rpc: (chain) => ({
-  //     http: "https://alpha-rpc.scroll.io/l2",
-  //   }),
-  // }),
   walletConnectProvider({
     projectId: connectWalletId,
   }),
