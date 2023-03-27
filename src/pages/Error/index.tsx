866ef1caa0c2d45066dc3183d79f43234c6a28fa
ankerx <93661548+ankerx@users.noreply.github.com>
2023-03-27 10:56:24 -0400

    redirect to error page when onfido status = error

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,5 +1,7 @@
+import { ArrowLeftOutlined } from "@ant-design/icons";
 import { Result } from "antd";
-import { Container } from "../../components/Mint/styled";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Error = () => {
   return (
@@ -7,11 +9,41 @@ const Error = () => {
       <Result
         status="error"
         title="Verification failed"
-        subTitle="For more information, please contact support."
-        extra={[<a href="mailto:support@0xkyc.id">support@0xkyc.id</a>]}
+        extra={
+          <Link to="/">
+            <ArrowLeftOutlined style={{ fontSize: ".75rem" }} /> Go to homepage
+          </Link>
+        }
       />
+      <TextBox>
+        <Text>
+          We weren't able to verify your request, please contact{" "}
+          <a href="mailto:support@0xkyc.id">support@0xkyc.id</a> and carefully
+          explain your situation and we will try to help. Please be advised it
+          might take up to 48h to get a response. Please don't share any
+          personal information with us except for your wallet address and the
+          blockchain and it's version (ex. Polygon Mumbai, Ethereum Goerli
+          etc.).
+        </Text>
+      </TextBox>
     </Container>
   );
 };
 
 export default Error;
+
+const Text = styled("p")`
+  color: #616161;
+`;
+const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 85vh;
+  padding-bottom: 15rem;
+`;
+const TextBox = styled("div")`
+  text-align: center;
+  max-width: 70%;
+`;
--- src/service/onfido/onfido.service.ts
+++ src/service/onfido/onfido.service.ts
@@ -21,6 +21,7 @@ export async function onfidoRedirect(
     redirectUrl: redirectUrl ? redirectUrl : window.location.href,
     email: email || "",
   });
+
   window.location.replace(res.data);
 }
 
