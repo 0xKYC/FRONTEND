0518b8d64b9a363a840561bbaf3a2e3d6b772512
ankerx <93661548+ankerx@users.noreply.github.com>
2023-05-16 07:32:50 -0400

    remove console log

--- src/components/Content/Verified/utils.ts
+++ src/components/Content/Verified/utils.ts
@@ -5,6 +5,6 @@ export const getUserSbt = (user: User, chainId: ChainId) => {
   if (!user.sbts) return null;
 
   const sbt = user.sbts.find((sbt) => sbt.chainId === chainId);
-  console.log(user.sbts);
+
   return sbt;
 };
