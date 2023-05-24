905b99d7ed5fc140d9d90129e40efcaebcbad031
ankerx <93661548+ankerx@users.noreply.github.com>
2023-05-24 17:47:54 -0400

    fix: fetching md files

--- src/common/TextContainer/useFetchContent.tsx
+++ src/common/TextContainer/useFetchContent.tsx
@@ -4,9 +4,11 @@ export const useFetchContent = (path: string) => {
   const [content, setContent] = useState("");
 
   useEffect(() => {
-    fetch(`legal/${path}.md`)
-      .then((res) => res.text())
-      .then((text) => setContent(text));
+    import(`../../content/legal/${path}.md`).then((res) => {
+      fetch(res.default)
+        .then((res) => res.text())
+        .then((text) => setContent(text));
+    });
   }, [path]);
 
   return { content };
