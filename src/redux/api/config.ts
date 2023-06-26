e02bd6a0a7b7ba06fceb44ab1eedb2735852d300
ankerx <93661548+ankerx@users.noreply.github.com>
2023-06-26 11:36:18 -0400

    change api url

--- src/redux/api/config.ts
+++ src/redux/api/config.ts
@@ -1,4 +1,2 @@
-// process.env.REACT_APP_BASE_URL || "http://localhost:3001/";
-export const API_URL = "http://localhost:3001/";
-
-// ("https://devapi.0xkyc.id");
+export const API_URL =
+  process.env.REACT_APP_BASE_URL || "http://localhost:3001/";
