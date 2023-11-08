160cc303753005243a0dbdacf0b3fe8bad4c0008
lucqaz01 <Lucassobczak3@gmail.com>
2023-11-07 22:27:16 -0500

    Added loom video + changed styling for mobile view

--- src/router/pages/Tutorials/index.tsx
+++ src/router/pages/Tutorials/index.tsx
@@ -14,18 +14,18 @@ const videos = [
   {
     title: "How to do Sunscreen",
     text,
-    videoSrc: "https://www.youtube.com/watch?v=QHm8tHxXlN4",
+    videoSrc: "https://www.loom.com/embed/5f260f8fcdd44461a1f61117f7ea1e8e?sid=0a148de9-78e3-4a5d-957d-73e3289cf4184",
   },
-  {
-    title: "How to do 0xKYC",
-    text,
-    videoSrc: "https://www.youtube.com/watch?v=QHm8tHxXlN4",
-  },
-  {
-    title: "How to add Discord Bot",
-    text,
-    videoSrc: "https://www.youtube.com/watch?v=QHm8tHxXlN4",
-  },
+  // {
+  //   title: "How to do 0xKYC",
+  //   text,
+  //   videoSrc: "https://www.youtube.com/watch?v=QHm8tHxXlN4",
+  // },
+  // {
+  //   title: "How to add Discord Bot",
+  //   text,
+  //   videoSrc: "https://www.youtube.com/watch?v=QHm8tHxXlN4",
+  // },
 ];
 
 const Tutorials = () => {
@@ -42,62 +42,89 @@ const Tutorials = () => {
 
   return (
     <div
-      style={{ marginTop: "3rem", paddingBottom: "10rem", textAlign: "center" }}
+      style={{ 
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "3rem",
+        padding: "0 0.5rem 10rem 0.5rem", 
+        textAlign: "center"
+      }}
     >
-      <Heading>Welcome on Tutorial Page!</Heading>
-
-      {isExploding && (
-        <ConfettiExplosion
-          onComplete={() => setIsExploding(false)}
-          duration={1800}
-          height="100vh"
-        />
-      )}
-      <div
-        style={{ display: "flex", flexDirection: "column", marginTop: "1rem" }}
-      >
+      <TutorialVideoSection>
+        <Heading>Welcome on Tutorial Page!</Heading>
+        {isExploding && (
+          <ConfettiExplosion
+            onComplete={() => setIsExploding(false)}
+            duration={1800}
+            height="100vh"
+          />
+        )}
+        <div
+          style={{ display: "flex", flexDirection: "column", marginTop: "1rem" }}
+        >
+          {videos.map((video, idx) => {
+            return (
+              <p
+                onClick={() => handleClickScroll(`section-${idx}`)}
+                style={{ cursor: "pointer", fontWeight: 400, fontSize: "1.1rem" }}
+              >
+                {idx + 1}. {video.title}
+              </p>
+            );
+          })}
+        </div>
+        <Divider />
         {videos.map((video, idx) => {
           return (
-            <p
-              onClick={() => handleClickScroll(`section-${idx}`)}
-              style={{ cursor: "pointer", fontWeight: 400, fontSize: "1.1rem" }}
+            <div
+              id={`section-${idx.toString()}`}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                gap: "1rem"
+              }}
             >
-              {idx + 1}. {video.title}
-            </p>
+              <Title>{video.title}</Title>
+              <p style={{ fontSize: "1.2rem" }}>{video.text}</p>
+              <div
+              style={{
+                position: "relative",
+                width: "100%",
+                //maintain aspect ratio of 16:9
+                paddingBottom: "56.25%" 
+              }}
+              >
+                <VideoFrame 
+                src={video.videoSrc}
+                />
+              </div>
+              <Divider />
+            </div>
           );
         })}
-      </div>
-      <Divider />
-      {videos.map((video, idx) => {
-        return (
-          <div
-            id={`section-${idx.toString()}`}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-              gap: "1rem",
-            }}
-          >
-            <Title>{video.title}</Title>
-            <p style={{ fontSize: "1.2rem" }}>{video.text}</p>
 
-            <ReactPlayer url={video.videoSrc} controls={true} />
-            <Divider />
-          </div>
-        );
-      })}
-
-      <InfoText>
-        If you encounter any difficulties, don't hesitate to reach out to{" "}
-        <a href="mailto:support@0xkyc.id">our support team</a>.
-      </InfoText>
+        <InfoText>
+          If you encounter any difficulties, don't hesitate to reach out to{" "}
+          <a href="mailto:support@0xkyc.id">our support team</a>.
+        </InfoText>
+      </TutorialVideoSection>
     </div>
   );
 };
 
 export default Tutorials;
+
+const TutorialVideoSection = styled.div`
+  width: 100%;
+  padding: 0px 18px;
+
+  @media screen and (min-width: 768px){
+    max-width: calc(100% - 38px)
+  }
+`;
 
 const Title = styled.h3`
   font-size: 1.7rem;
@@ -107,4 +134,15 @@ const Title = styled.h3`
     font-size: 1.9rem;
     margin-top: 1.5rem;
   }
+`;
+
+const VideoFrame = styled.iframe`
+  position: absolute;
+  top: 0;
+  right: 50%;
+  bottom: 50%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
 `;
