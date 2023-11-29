af17db9c1aa280a46e5ddad962064a750007a59c
ankerx <93661548+ankerx@users.noreply.github.com>
2023-11-29 16:24:23 +0100

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,30 @@ import react from "@vitejs/plugin-react";
 
 import { defineConfig } from "vite";
 
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser", //fix production build
+    },
+  },
   // depending on your application, base can also be "/"
   base: "",
   plugins: [react(), viteTsconfigPaths()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodeResolve({
+          browser: true,
+          preferBuiltins: false,
+        }),
+        json(),
+      ],
+    },
+  },
   server: {
     // this ensures that the browser opens upon server start
     open: true,
